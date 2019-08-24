7a9ccd157a20fbcf0aeed1fbbbe2cf820081a703
Makiasake <alexandre.maki@hotmail.com>
2019-08-23 23:06:01 -0300

    insert implementation

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,10 +1,10 @@
 package application;
 
+import java.util.Date;
 import java.util.List;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
-import model.dao.impl.SellerDaoJDBC;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -12,20 +12,28 @@ public class Main {
 
 	public static void main(String[] args) {
 		
+		SellerDao sellerDao = DaoFactory.createSellerDao(); 
+		
 		System.out.println("Seller findById");
-		Seller seller = DaoFactory.createSellerDao().findById(2);
+		Seller seller = sellerDao.findById(2);
 		System.out.println(seller);
 		
 		System.out.println("Seller findByDepartment");
-		List<Seller> list = DaoFactory.createSellerDao().findByDepartment(new Department(2, null));
+		Department dep = new Department(2, null);
+		List<Seller> list = sellerDao.findByDepartment(dep);
 		for (Seller s: list) {
 			System.out.println(s);
 		}
 		
 		System.out.println("Seller findAll");
-		list = DaoFactory.createSellerDao().findAll();
+		list = sellerDao.findAll();
 		for (Seller s: list) {
 			System.out.println(s);
 		}
+		
+		System.out.println("Seller insert");
+		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, dep);
+		sellerDao.insert(newSeller);
+		System.out.println("Inserted new id: " + newSeller.getId());
 	}
 }
